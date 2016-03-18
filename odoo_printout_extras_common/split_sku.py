e41bef048c979fcedfeb101bfa668992795e09bc
Nick Booker <nick.booker@opusvl.com>
2016-03-18 15:21:22 +0000

    Create split SKU mixin for most common case

---
+++ odoo_printout_extras_common/split_sku.py
@@ -0,0 +1,28 @@
+
+"""Split SKU common code.
+"""
+
+from openerp import fields, api
+
+class SplitSKUFieldsMixin(object):
+    description_without_sku = fields.Char(
+        compute="_description_without_sku_compute",
+        readonly=True,
+        string='Description without our product code',
+    )
+
+    product_sku = fields.Char(
+        related=['product_id', 'default_code'],
+        readonly=True,
+        string='Our Product Code',
+    )
+
+    @api.depends('name', 'product_sku')
+    @api.one
+    def _description_without_sku_compute(self):
+        if self.product_sku and self.name:
+            sku_prefix = '[%s] ' % (self.product_sku,)
+            self.description_without_sku = self.name.replace(sku_prefix, '')
+        else:
+            self.description_without_sku = self.name
+
--- purchase_order_split_sku_fields/models.py
+++ purchase_order_split_sku_fields/models.py
@@ -1,28 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from openerp import models, fields, api
+from openerp import models
+from ..odoo_printout_extras_common.split_sku import SplitSKUFieldsMixin
 
-class PurchaseOrderLine(models.Model):
+class PurchaseOrderLine(models.Model, SplitSKUFieldsMixin):
     _inherit = 'purchase.order.line'
-
-    description_without_sku = fields.Char(
-        compute="_description_without_sku_compute",
-        readonly=True,
-        string='Description without our product code',
-    )
-
-    product_sku = fields.Char(
-        related=['product_id', 'default_code'],
-        readonly=True,
-        string='Our Product Code',
-    )
-
-    @api.depends('name', 'product_sku')
-    @api.one
-    def _description_without_sku_compute(self):
-        if self.product_sku and self.name:
-            sku_prefix = '[%s] ' % (self.product_sku,)
-            self.description_without_sku = self.name.replace(sku_prefix, '')
-        else:
-            self.description_without_sku = self.name
-
